770cd9c5434edcbc98dcebf9c26264d609f0da74
jayvanzyl <jay@ecosystem.ai>
2025-03-17 09:17:22 +0200

    build updates

--- 01-build.sh
+++ 01-build.sh
@@ -1,2 +1,2 @@
 docker build --platform linux/amd64 -t ecosystemai/ecosystem-algorithms:latest . && \
-docker build --platformlinux/arm64 -t ecosystemai/ecosystem-algorithms:arm64 .
+docker build --platform linux/arm64 -t ecosystemai/ecosystem-algorithms:arm64 .
